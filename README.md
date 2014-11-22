6b4d40b9b8593753f402600ba2884eb7c15cd8e0
curtisz <czimmerman@ezoic.com>
2014-11-22 01:24:33 -0800

    added: more precise usage info, stub functions in primary module

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 
 Garbage-in-garbage-out: Soviet-style storage.
 
+###details
+
+gigo is "soviet-style storage" because it is very practical. it does not have a lot of 
+bells and whistles and isn't terribly efficient or fast. it does what it says on the 
+tin; no more, no less.
+
 ###installation
 
 ```sh
@@ -11,12 +17,52 @@ npm install gigo
 
 ###usage
 
+gigo just needs to be required like any module:
+
 ```javascript
 var gigo = require("gigo");
+```
 
+gigo can be used for very simple storage:
+
+```javascript
 gigo.set("name", "gigo");
 console.log( "name is: " + gigo.get("name") );
 // name is: gigo
+```
+
+it's aware of different kinds of data...
+
+```javascript
+var result = {
+	bar: 0,
+	foo: true
+};
+gigo.set("serialized", result);
+var restored = gigo.get("serialized");
+if( restored.foo === true ) {
+	console.log("foo is true!");
+	// foo is true!
+}
+gigo.remove("serialized");
+console.log( typeof( gigo.get("serialized") ) );
+// undefined
+```
+
+...but it keeps to its original promise of returning what it was given
+
+```javascript
+var send = function( client ) {
+	var pic = gigo.get("pic");
+	// ... send to client via HTTP response or whatever
+	client.send(pic);
+};
+var file = function( filename ) {
+	fs.readFile(filename, function(err, data) {
+		if (err) return console.log(err), err;
+		gigo.set("pic", data);
+	});
+};
 ```
 
 ###license
--- gigo.js
+++ gigo.js
@@ -9,30 +9,70 @@
  * @version 0.0.1a
  */
 
-/**********************************************
- Installation:
- npm install gigo
-
- Usage:
- var gigo = require('gigo');
-**********************************************/
-
 var exports = module.exports = __gigo = (function() {
 	"use strict";
 
-	var $data = {};
+	var redis = require('redis');
+
+	var $data = {
+		client: redis.createClient()
+	};
+
+	var $lib = {
+		redis: require('redis')
+	};
 
 	var $func = {
-		get: function( key ) {
+		close: function( db ) {
+			// close the specified database
 			return false;
 		},
-		set: function( key, value ) {
+		config: function( descriptor ) {
+			// config this isntance of gigo
+			return false;
+		},
+		drop: function( db ) {
+			// drop the specified database
+			return false;
+		},
+		get: function( key, callback ) {
+			// get the data associated with the specified key
+			return false;
+		},
+		open: function( db ) {
+			// open the specified database
+			return false;
+		},
+		remove: function( key ) {
+			// remove the specified key and associated data
+			return false;
+		},
+		set: function( key, value, callback ) {
+			// set the data associated with a specified key
+			return false;
+		}
+	};
+
+	var $util = {
+		base64: function( string, type ) {
+			return false;
+		},
+		genGUID: function() {
+			return false;
+		},
+		genID: function( len ) {
 			return false;
 		}
 	};
 
 	return {
+		close: $func.close,
+		config: $func.config,
+		drop: $func.drop,
 		get: $func.get,
+		open: $func.open,
+		remove: $func.remove,
+		save: $func.save,
 		set: $func.set
 	};
 })();
